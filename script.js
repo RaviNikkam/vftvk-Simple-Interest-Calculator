c812805382cec291f5e50ce63f957418b53295d9
Ravi NIKKAM <nikkam.ravikumar@gmail.com>
2021-04-29 08:54:24 +0000

    Added validation code and comments

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+// A function to compute simple interest
 function compute()
 {
     var principal = document.getElementById("principal").value;
+
+    // Code to validate principal input field
+    if(!Number.isInteger(principal) && principal <= 0){
+            alert("Please enter positive value greater than zero");
+            document.getElementById("principal").focus();
+            return false;
+    }
+
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
@@ -11,9 +20,9 @@ function compute()
                                                 "in the year <span class='highlight'>"+year+"</span>.";
 }
         
+// A function to dynamically update the rate of interest selected using the range button.
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-
